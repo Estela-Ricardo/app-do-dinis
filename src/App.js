e528ee9c1e036e18bd0bb3b10b2c1bb8c08d29c9
Estela Ricardo <62676073+Estela-Ricardo@users.noreply.github.com>
2023-02-18 15:09:45 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import joke from './joke.json';
 import './App.css';
 
-function App() {
+
+const App = () => {
+  const [showAnswer, setShowAnswer] = useState(false);
+  const [currentJoke, setCurrentJoke] = useState(joke[Math.floor(Math.random() * joke.length)]);
+
+  const handleClick = () => {
+    setShowAnswer(!showAnswer);
+  };
+
+
+  const handleNewJokeClick = () => {
+    setCurrentJoke(joke[Math.floor(Math.random() * joke.length)]);
+    setShowAnswer(false);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="joke-app">
+      <div className="content">
+        <h1>Livro de Piadas do Dinis</h1>
+        <p>{currentJoke.question}</p>
+        {showAnswer ? <div dangerouslySetInnerHTML={{ __html: currentJoke.answer }} /> : <div style={{ height: '1.2em' }} >&nbsp;</div>}
+        <button onClick={handleClick}>
+          {showAnswer ? 'Esconder Resposta' : 'Mostrar Resposta'}
+        </button>
+
+        <button onClick={handleNewJokeClick}>Nova Piada</button>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default App
